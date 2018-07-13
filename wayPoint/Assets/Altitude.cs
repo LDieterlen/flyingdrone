56b9746484e7b80a96bc4d0add107ee7b9512f50
herveantony <rvantony@gmail.com>
2018-07-13 16:16:27 +0200

    Changements Herve

---
+++ wayPoint/Assets/Altitude.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Altitude : MonoBehaviour {
+    public Collider terrain;
+ 
+    public GameObject cube;
+    private int countWayPoints;
+    private int i=3;
+    public GameObject[] waypoints;
+    private bool allowPoint;
+    private Vector3 tempPosition;
+
+
+    // Use this for initialization
+    void Start () {
+    
+        terrain =GetComponent<Collider>();
+        countWayPoints = 7;
+        allowPoint = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+      
+         if (Input.GetMouseButtonDown(0))
+         {
+
+            if (allowPoint)
+            {
+                if (i < 7)
+                {
+                    RaycastHit hit;
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    if (terrain.Raycast(ray, out hit, Mathf.Infinity))
+                    {
+
+                     
+                        tempPosition = hit.point;                    
+                        waypoints[i].transform.position = tempPosition;                
+                        StartCoroutine(Timer());
+
+                    }
+                }
+                if (i >= 6)
+                {
+                    allowPoint = false;
+                }
+            }
+        }
+    }
+
+
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(0.4f);
+        i++;
+      
+    }
+
+  }
---
+++ wayPoint/Assets/Waypoints.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour {
+
+    public GameObject[] waypoints;
+    int current = 0;
+    float rotSpeed;
+    public static float speed = 0f;
+    float WPradius = 1;
+  
+
+
+
+    // Use this for initialization
+    void Start () {
+   
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Vector3.Distance(waypoints[current].transform.position, transform.position) < WPradius)
+        {
+
+            current++;
+            if (current >= waypoints.Length)
+            {
+                current = 0;
+            }
+            Vector3 relativePos = waypoints[current].transform.position - transform.position;        
+            Quaternion rotation = Quaternion.LookRotation(relativePos);
+            transform.rotation = rotation;
+          
+
+        }
+
+
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
+        
+
+
+
+  
+
+
+
+    }
+}
